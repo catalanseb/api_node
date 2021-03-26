32d00d36f31e1243057814115f7f667dae0845d9
Sebastian Catalan Araya <catalan.seb@gmail.com>
2021-03-26 14:29:13 -0300

    fix routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 module.exports = function({ ProductController }) {
     const router = Router();
 
-    router.get("/all", ProductController.getAll);
+    router.get("", ProductController.getAll);
     router.get("/:productId", ProductController.get);
     router.post("", ProductController.create);
     router.patch("/:productId", ProductController.update);
